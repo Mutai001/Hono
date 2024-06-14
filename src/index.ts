aec379938b886830343744e998ca1c1d112beef8
Mutai001 <omusekim@gmail.com>
2024-06-14 17:31:12 +0300

    added name

--- src/index.ts
+++ src/index.ts
@@ -129,7 +129,7 @@ app.get('/', (c) => {
     <h1>Welcome to Our Restaurant API</h1>
   </header>
   <main>
-    <h1>Hello, ICyrus Kimutai 😊</h1>
+    <h1>Hello, Im Cyrus Kimutai 😊</h1>
 
     <p>Welcome to our restaurant API. Here you can find a variety of services to manage your restaurant data.</p>
   </main>
